207abee2dc343ed0ce6725115993b538d5b03d2f
Aakaive <wonseok031@gmail.com>
2024-08-28 10:57:08 +0900

    페이지네이션 구현

--- src/main/java/com/sparta/springwork02/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/springwork02/repository/ScheduleRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.springwork02.repository;
 
 import com.sparta.springwork02.entity.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
-
+    Page<Schedule> findAllByOrderByModifiedAtDesc(Pageable pageable);
 }
